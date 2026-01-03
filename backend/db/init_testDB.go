c9c15dfc6ecdb7abb2eea73a345bbeaafc1d9234
Kanadesisido <takasuyari@gmail.com>
2026-01-03 21:50:20 +0900

    test db changed sqlite -> mysql

--- backend/db/init_testDB.go
+++ backend/db/init_testDB.go
@@ -2,26 +2,37 @@ package db
 
 import (
 	"ameato/model"
+	"fmt"
+	"os"
 	"testing"
 
-	"gorm.io/driver/sqlite"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitTestDB(t *testing.T) *gorm.DB {
 	t.Helper()
 
-	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	err := godotenv.Load("../.env")
+	if err != nil {
+		t.Fatalf("failed to load environment variables: %v", err)
+	}
+
+	dsn := createTestDsn(os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_TEST_NAME"))
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		t.Fatalf("failed to connect database: %v", err)
-		return nil
 	}
 
 	if err := db.AutoMigrate(&model.Message{}); err != nil {
 		t.Fatalf("failed to migrate database: %v", err)
-		return nil
 	}
 
 	return db
+}
+
+func createTestDsn(username string, password string, dbname string) string {
+	return fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, dbname)
 }

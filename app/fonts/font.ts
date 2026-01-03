7c020c8aafbbf86c77f6f84412e158ba7069f415
Kanadesisido <takasuyari@gmail.com>
2026-01-03 18:17:47 +0900

    apply: font

---
+++ app/fonts/font.ts
@@ -0,0 +1,13 @@
+import { Josefin_Sans, M_PLUS_Rounded_1c } from "next/font/google";
+
+export const MPLUSRounded1c = M_PLUS_Rounded_1c({
+	subsets: ["latin"],
+	weight: ["100", "400", "700"],
+	variable: "--font-mplus-rounded-1c",
+});
+
+export const JosefinSans = Josefin_Sans({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-josefin-sans",
+});

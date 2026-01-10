dfa2790da617bc473e2dc62c531aaba9e0101248
Kanadesisido <takasuyari@gmail.com>
2026-01-10 18:19:17 +0900

    update: context + fade

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-	title: "ame:ato | draw your condensed messages.",
-	description: "ame:ato - 雨の日の窓みたいなSNSアプリケーション",
-};
-
 export const viewport: Viewport = {
 	width: "device-width",
 	initialScale: 1,
 	maximumScale: 1,
 	userScalable: false,
+};
+
+export const metadata: Metadata = {
+	title: "ame:ato | draw your condensed messages.",
+	description: "ame:ato - 雨の日の窓みたいなSNSアプリケーション",
 };
 
 export default function RootLayout({
@@ -22,12 +22,8 @@ export default function RootLayout({
 		<html lang='jp' className='scrollbar-hidden'>
 			<head>
 				<meta name='referrer' content='strict-origin-when-cross-origin' />
-				{/* <meta
-					http-equiv='Content-Security-Policy'
-					content={`default-src 'self'; connect-src 'self' ${process.env.NEXT_PUBLIC_API_URL};`}
-				/> */}
 			</head>
-			<body className='antialiased'>{children}</body>
+			<body className='antialiased bg-gray-800'>{children}</body>
 		</html>
 	);
 }

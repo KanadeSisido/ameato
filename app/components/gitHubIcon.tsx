0dead2959fd52013d6a23dce7c3cdf3cd652047e
Kanadesisido <takasuyari@gmail.com>
2026-01-10 18:19:43 +0900

    gh-icon

---
+++ app/components/gitHubIcon.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const GitHubIconTips = () => {
+	return (
+		<Image
+			src='/github-mark-white.svg'
+			alt='GitHub Icon'
+			width={32}
+			height={32}
+		/>
+	);
+};
+
+export default GitHubIconTips;
